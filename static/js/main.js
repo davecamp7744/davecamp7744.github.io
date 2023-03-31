5b890d447364b4fc9e601c8fad62899084b1b074
davecamp7744 <92772510+davecamp7744@users.noreply.github.com>
2023-03-30 23:33:08 -0600

    Site
    Subida de archivos

---
+++ static/js/main.js
@@ -0,0 +1,53 @@
+//FUNCION PARA EL TEXTO
+function send_message(message) {
+$.ajax({
+    type: 'POST',
+    url: '/message',
+    data: { 'message': message },
+    success: function (response) {
+    $('#chat').append('<div>' + response + '</div>');
+    }
+});
+}
+
+
+//OCULTAR BUTTON
+const button = document.querySelector('#mi-boton');
+button.addEventListener('click', () => {
+    button.style.display = 'none';
+});
+const btnenvi = document.querySelector('#btn-enviar');
+btnenvi.addEventListener('click', () => {
+    btnenvi.style.display = 'none';
+})
+// const op = document.querySelector('#botones');
+// op.addEventListener('click', () => {
+//     op.style.display = 'none';
+// })
+//FUNCION PARA EL NOMBRE
+function send() {
+    var userinput = document.getElementById("userinput").value;
+    document.getElementById("chatbox").innerHTML += "<p><strong>Tú:</strong> " + userinput + "</p>";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("chatbox").innerHTML += "<p><strong>Bot:</strong> " + this.responseText + "</p>";
+        }
+    };
+    xhttp.open("GET", "/get?msg=" + userinput, true);
+    xhttp.send();
+    document.getElementById("userinput").value = "";
+};
+
+
+// const inpu = document.querySelector('#userinput');
+// inpu.addEventListener('click', () => {
+//     inpu.style.display = 'none';
+// });
+
+// var nombre = null;
+// var status = null;
+// function sub(){
+// nombre = document.getElementsByName("nombre")[0].value;
+// alert("Tu nombre es: ",nombre);
+// };
